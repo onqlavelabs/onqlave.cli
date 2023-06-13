a34d47538924ad8a22adaf314eb021fad380181e
Desmond-Onqlave <119550866+Desmond-Onqlave@users.noreply.github.com>
2023-06-13 15:24:43 +0700

    Clean Up Orphan Code (#8)

--- internal/pkg/cli/api/url.go
+++ internal/pkg/cli/api/url.go
@@ -22,14 +22,6 @@ func IsLocal() bool {
 	return viper.GetString("env") == "local"
 }
 
-func IsDev() bool {
-	return viper.GetString("env") == "dev"
-}
-
-func IsProd() bool {
-	return viper.GetString("env") == "prod"
-}
-
 func GetBaseUrl(serviceName string) string {
 	const defaultURL = "http://localhost:8081"
 	apiBaseUrl := viper.GetStringMapString("api_base_url")
@@ -38,9 +30,11 @@ func GetBaseUrl(serviceName string) string {
 	}
 	return defaultURL
 }
+
 func UrlBuilder(serviceName string) string {
 	if IsLocal() {
 		return fmt.Sprintf("%s/%s", GetBaseUrl(serviceName), serviceName)
 	}
+
 	return fmt.Sprintf("%s/%s", viper.GetString("api_base_url"), serviceName)
 }
