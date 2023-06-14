4b9a5dd154b2d08fb9672a0f97ef839a8fbd4273
Desmond <desmond@onqlave.com>
2023-06-14 16:22:42 +0700

    Feature: Rename package files

--- core/contracts/arx/requests/create.go
+++ core/contracts/arx/requests/create.go
@@ -1,6 +1,8 @@
 package requests
 
-import "github.com/onqlavelabs/onqlave.cli/core/contracts/arx"
+import (
+	"github.com/onqlavelabs/onqlave.cli/core/contracts/arx"
+)
 
 type AddClusterRequest struct {
 	Cluster contracts.NewCluster `json:"cluster" validate:"required"`
--- core/contracts/arx/requests/update.go
+++ core/contracts/arx/requests/update.go
@@ -1,6 +1,8 @@
 package requests
 
-import "github.com/onqlavelabs/onqlave.cli/core/contracts/arx"
+import (
+	"github.com/onqlavelabs/onqlave.cli/core/contracts/arx"
+)
 
 type UpdateClusterRequest struct {
 	Cluster contracts.UpdateCluster `json:"cluster" validate:"required"`
